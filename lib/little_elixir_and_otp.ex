347a88a10beed6f0b3dd0bae0d18b5dc934fd4a5
Ed Gibbs <edward_gibbs@yahoo.com>
2016-12-08 21:59:56 -0800

    Add some guard clauses to meter conversions

--- lib/little_elixir_and_otp.ex
+++ lib/little_elixir_and_otp.ex
@@ -1,7 +1,7 @@
 defmodule LittleElixirAndOtp do
   defmodule MeterToLengthConverter do
-    def convert(:inch, meters), do: meters * 39.3701
-    def convert(:feet, meters), do: meters * 3.28084
-    def convert(:yard, meters), do: meters * 1.09361
+    def convert(:inch, meters) when is_number(meters) and meters >= 0, do: meters * 39.3701
+    def convert(:feet, meters) when is_number(meters) and meters >= 0, do: meters * 3.28084
+    def convert(:yard, meters) when is_number(meters) and meters >= 0, do: meters * 1.09361
   end
 end
--- test/little_elixir_and_otp_test.exs
+++ test/little_elixir_and_otp_test.exs
@@ -6,12 +6,36 @@ defmodule LittleElixirAndOtpTest do
       assert LittleElixirAndOtp.MeterToLengthConverter.convert(:feet, 1) == 3.28084
     end
 
-    test "converts one meter to inches properly" do
+    test "converts one meter to inches properly and only with numbers" do
       assert LittleElixirAndOtp.MeterToLengthConverter.convert(:inch, 1) == 39.3701
     end
 
-    test "converts one meter to yards properly" do
+    test "converts one meter to yards properly and only with numbers" do
       assert LittleElixirAndOtp.MeterToLengthConverter.convert(:yard, 1) == 1.09361
+    end
+
+    test "fails to convert non-number" do
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:feet, "smelly")
+      end
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:inch, "smelly")
+      end
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:yard, "smelly")
+      end
+    end
+
+    test "fails to convert negative numbers" do
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:feet, -1)
+      end
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:inch, -2)
+      end
+      assert_raise FunctionClauseError, fn ->
+        LittleElixirAndOtp.MeterToLengthConverter.convert(:yard, -3)
+      end
     end
   end
 end
