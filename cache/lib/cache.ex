ef089914cd40b6bab1f6d2d552a1648df3059934
Ed Gibbs <edward_gibbs@yahoo.com>
2016-12-25 07:21:38 -0800

    Deletes cache records

--- cache/lib/cache.ex
+++ cache/lib/cache.ex
@@ -3,6 +3,10 @@ defmodule Cache do
 
   ## Client API
 
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: :cache_worker)
+  end
+
   def write(key, value) do
     GenServer.cast(:cache_worker, {:write, key, value})
   end
@@ -11,14 +15,18 @@ defmodule Cache do
     GenServer.call(:cache_worker, {:read, key})
   end
 
-  def start_link do
-    GenServer.start_link(__MODULE__, %{}, name: :cache_worker)
+  def delete(key) do
+    GenServer.cast(:cache_worker, {:delete, key})
   end
 
   ## Server callbacks
 
   def handle_cast({:write, key, value}, state) do
     {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_cast({:delete, key}, state) do
+    {:noreply, Map.delete(state, key)}
   end
 
   def handle_call({:read, key}, _from, state) do
--- cache/test/cache_test.exs
+++ cache/test/cache_test.exs
@@ -6,4 +6,12 @@ defmodule CacheTest do
     Cache.write(:stooges, ["Larry", "Curly", "Moe"])
     assert Cache.read(:stooges) == ["Larry", "Curly", "Moe"]
   end
+
+  test "can delete a cache item" do
+    Cache.start_link
+    Cache.write(:stooges, ["Larry", "Curly", "Moe"])
+    assert Cache.read(:stooges) == ["Larry", "Curly", "Moe"]
+    Cache.delete(:stooges)
+    assert Cache.read(:stooges) == nil
+  end
 end
