ce274468778f245444d5e5b6dc11665e1434f5e6
Ed Gibbs <edward_gibbs@yahoo.com>
2017-01-03 17:26:19 -0800

    Implement prepopulation of pool.

--- pooly/lib/pooly/server.ex
+++ pooly/lib/pooly/server.ex
@@ -1,6 +1,6 @@
 defmodule Pooly.Server do
   use GenServer
-  # import Server.Spec
+  import Supervisor.Spec
 
   defmodule State do
     defstruct sup: nil, size: nil, mfa: nil
@@ -37,6 +37,38 @@ defmodule Pooly.Server do
   def init([], state) do
     send(self, :start_worker_supervisor)
     {:ok, state}
+  end
+
+  def handle_info(:start_worker_supervisor, state = %{sup: sup, mfa: mfa, size: size}) do
+    {:ok, worker_sup} = Supervisor.start_child(sup, supervisor_spec(mfa))
+    workers = prepopulate(size, worker_sup)
+    {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
+  end
+
+  ###########
+  # Private #
+  ###########
+
+  defp prepopulate(size, sup) do
+    prepopulate(size, sup, [])
+  end
+
+  defp prepopulate(size, _sup, workers) when size < 1 do
+    workers
+  end
+
+  defp prepopulate(size, sup, workers) do
+    prepopulate(size - 1, sup, [new_worker(sup) | workers])
+  end
+
+  defp new_worker(sup) do
+    {:ok, worker} = Supervisor.start_child(sup, [[]])
+    worker
+  end
+
+  defp supervisor_spec(mfa) do
+    opts = [restart: :temporary]
+    supervisor(Pooly.WorkerSupervisor, [mfa], opts)
   end
 
 end
